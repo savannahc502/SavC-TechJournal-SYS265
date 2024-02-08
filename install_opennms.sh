57f46a83917c30ef217379c254b08b9848fa0cc0
Savannah C <113316987+savannahc502@users.noreply.github.com>
2024-02-08 17:57:17 -0500

    Update install_opennms.sh

--- install_opennms.sh
+++ install_opennms.sh
@@ -1,4 +1,4 @@
-n#!/bin/bash
+#!/bin/bash
 
 
 # Must run the script as root or sudo-user. Tells the user this. 
@@ -13,16 +13,16 @@ if [ "$user_answer" == "y" ] || [ "$user_answer" == "Y" ]; then
 	yum -y update
 
 	# Install the postgresql Repository
-	sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+	yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 
 	# Install postgresql-12
-	sudo yum -y install postgresql12-server postgresql12
+	yum -y install postgresql12-server postgresql12
 
 	# Initialize postgresql-12
-	sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
+	/usr/pgsql-12/bin/postgresql-12-setup initdb
       
 	# Enable postgresql-12
-	sudo systemctl enable --now postgresql-12
+	systemctl enable --now postgresql-12
 
 	# Create new postgresql user "opennms"
 	sudo -i -u postgres psql -c "CREATE USER opennms WITH PASSWORD 'opennms';" 
@@ -33,8 +33,12 @@ if [ "$user_answer" == "y" ] || [ "$user_answer" == "Y" ]; then
 	# Change password for the postgres sudo user 
 	sudo -i -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
+ 	# Editing postgresql-12 file hashing 
+	sed '86 s/ident/md5/' /var/lib/pgsql/12/data/pg_hba.conf
+ 	sed '88 s/ident/md5/' /var/lib/pgsql/12/data/pg_hba.conf
+
 	# Reload postgresql-12
-	sudo systemctl reload postgresql-12
+	systemctl reload postgresql-12
 
 	# Install opennms repository
 	yum -y install https://yum.opennms.org/repofiles/opennms-repo-stable-rhel7.noarch.rpm
@@ -46,41 +50,44 @@ if [ "$user_answer" == "y" ] || [ "$user_answer" == "Y" ]; then
 	yum -y install opennms
 
 	# Install epel-release
-	sudo yum -y install epel-release
+	yum -y install epel-release
 
 	# Install R-core
-	sudo yum -y install R-core
+	yum -y install R-core
 
 	# Install yum-utils
-	sudo yum -y install yum-utils
+	yum -y install yum-utils
 
 	# Disable opennms automatic updates
-	sudo yum-config-manager --disable opennms-repo-stable-*
+	yum-config-manager --disable opennms-repo-stable-*
+
+ 	# Editing credentials
+  	-u opennms sed /opt/opennms/etc/opennms-datasources.xml
 
 	# Setting up Java
-	sudo /opt/opennms/bin/runjava -s
-	sudo /opt/opennms/bin/install -dis
+	/opt/opennms/bin/runjava -s
+	/opt/opennms/bin/install -dis
 
 	# Enable opennms
-	sudo systemctl enable --now opennms
+	systemctl enable --now opennms
 
 	# Add masquerade to the firewall
-	sudo firewall-cmd --permanent --add-masquerade
+	firewall-cmd --permanent --add-masquerade
 
 	# Add port 8980/tcp to the firewall 
-	sudo firewall-cmd --permanent --add-port=8980/tcp
+	firewall-cmd --permanent --add-port=8980/tcp
 
 	# Add port 162/udp to the firewall 
-	sudo firewall-cmd --permanent --add-port=162/udp
+	firewall-cmd --permanent --add-port=162/udp
 
 	# Add port 10162/udp to the firewall
-	sudo firewall-cmd --permanent --add-port=10162/udp
+	firewall-cmd --permanent --add-port=10162/udp
 
 	# Fowarding port 162/udp to 10162/udp
-	sudo firewall-cmd --permanent --add-forward-port=port=162:proto=udp:toport=10162
+	firewall-cmd --permanent --add-forward-port=port=162:proto=udp:toport=10162
 
 	# Reload the firewall
-	sudo systemctl reload firewalld
+	systemctl reload firewalld
 
 	echo "OpenNMS and its dependencies has been installed successfully." 
 	echo "Please edit the two listed files below with the new credentials for the users created in this script and the hash md5 for local ipv4/6. After, reload opennms and postgresql-12"
