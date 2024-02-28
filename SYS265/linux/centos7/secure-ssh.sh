9054ce310639597b042af6caafa401e6bcc0cb2c
savannahc502 <savannah.ciak@mymail.champlain.edu>
2024-02-28 01:08:44 +0000

    update ssh script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -8,7 +8,7 @@
 # Creates a user and adds the public key
 sudo useradd -m -d /home/${1} -s /bin/bash ${1}
 sudo mkdir /home/${1}/.ssh
-cd ~/SavC-TechJournal-SYS265
+cd /home/savannah/SavC-TechJournal-SYS265
 sudo cp SYS265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
 sudo chmod 700 /home/${1}/.ssh
 sudo chmod 600 /home/${1}/.ssh/authorized_keys
